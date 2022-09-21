8aa8322ba9bb7d6357f9dc9f5ab90edcc340f741
Андрей Десюкевич <70821681+AndreyDesukevic@users.noreply.github.com>
2022-09-21 20:56:50 +0300

    UpdateAut

--- ListOfContacts/Controllers/TheFirstPageController.cs
+++ ListOfContacts/Controllers/TheFirstPageController.cs
@@ -1,97 +1,33 @@
-﻿using ListOfContacts.EfStuff.DbModel;
-using ListOfContacts.EfStuff.DbRepositories;
+﻿using ListOfContacts.EfStuff.DbRepositories;
 using ListOfContacts.Models;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using ListOfContacts.EfStuff;
-using Microsoft.EntityFrameworkCore;
 
 namespace ListOfContacts.Controllers
 {
-    public class AccountController : Controller
+    public class TheFirstPageController : Controller
     {
         private readonly ContactRepository _contactRepository;
 
-        private readonly WebDbContext db;
-        public AccountController(ContactRepository contactRepository, WebDbContext context)
+        public TheFirstPageController(ContactRepository contactRepository)
         {
-            db = context;
             _contactRepository = contactRepository;
         }
 
-        [HttpGet]
-        public IActionResult Login()
+        [Authorize]
+        public IActionResult Index()
         {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            if (ModelState.IsValid)
+            var dbContacts = _contactRepository.GetAll();
+            var viewModel = dbContacts.Select(dbContact => new ContactViewModel
             {
-                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
-                if (user != null)
-                {
-                    await Authenticate(model.Email); // аутентификация
-
-                    return RedirectToAction("Index", "Home");
-                }
-                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
-            }
-            return View(model);
-        }
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
-                if (user == null)
-                {
-                    // добавляем пользователя в бд
-                    db.Users.Add(new User { Email = model.Email, Password = model.Password });
-                    await db.SaveChangesAsync();
-
-                    await Authenticate(model.Email); // аутентификация
-
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
-            }
-            return View(model);
-        }
-
-        private async Task Authenticate(string userName)
-        {
-            // создаем один claim
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
-            };
-            // создаем объект ClaimsIdentity
-            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-            // установка аутентификационных куки
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Login", "Account");
+                Id = dbContact.Id,
+                Name = dbContact.Name,
+                MobilePhone = dbContact.MobilePhone,
+                JobTitle=dbContact.JobTitle,
+                BirthDate = dbContact.BirthDate,
+            }).ToList();
+            return View(viewModel);
         }
     }
 }
-
